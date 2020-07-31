ecac7853a173400f9bc0ada5fdc73a2745a62231
Frederik Trostorf <frederik.trostorf@tx.group>
2020-07-31 20:42:16 +0200

    use real userid and log exception

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
@@ -4,6 +4,7 @@ import com.udacity.jwdnd.course1.cloudstorage.mapper.FileMapper;
 import com.udacity.jwdnd.course1.cloudstorage.model.File;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,24 +13,24 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class FileService {
 
     private final FileMapper fileMapper;
 
     public void store(MultipartFile file, User user) {
-
         try {
             File newFile = File.builder()
                     .fileData(file.getBytes())
                     .contentType(file.getContentType())
                     .fileSize(file.getSize())
                     .fileName(file.getOriginalFilename())
-                    .userId(1)
+                    .userId(user.getUserId())
                     .build();
 
             fileMapper.insert(newFile);
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("Unable to get data from file {}.", file.getOriginalFilename());
         }
     }
 
